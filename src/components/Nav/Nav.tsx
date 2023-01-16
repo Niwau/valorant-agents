07bc52cb0bc6959f438718fb083864abc199620d
Guilherme Ribeiro <xxguilhermesoaresxx@gmail.com>
2023-01-16 00:08:22 -0300

    Reduzindo tamanho da fonte do menu de navegação

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -29,13 +29,13 @@ const styles = StyleSheet.create({
   },
   navText: {
     marginRight: 16,
-    fontSize: 20,
+    fontSize: 16,
     color: 'white',
     letterSpacing: 3
   },
   navTextInactive: {
     marginRight: 16,
-    fontSize: 20,
+    fontSize: 16,
     color: 'white',
     letterSpacing: 3,
     opacity: 0.6,
